8cb20a6797b157b4be6fd151cc896fa8e8718537
Ahmed <your-email@example.com>
2026-03-27 02:43:09 +0200

    custom widget in file upload video

---
+++ lib/features/uploadVideo/widgets/container_widg.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:firstversion1/shared/custom_text.dart';
+
+class CustomContainer extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const CustomContainer({Key? key, required this.title, required this.subtitle})
+    : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 123,
+      width: 319,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CustomText(
+            text: title,
+            size: 23,
+            color: const Color(0xff1E1E1E),
+            weight: FontWeight.w700,
+          ),
+          const Gap(6),
+          CustomText(
+            textAlign: TextAlign.center,
+            text: subtitle,
+            size: 18,
+            color: const Color(0xff6C6969),
+            weight: FontWeight.w400,
+          ),
+        ],
+      ),
+    );
+  }
+}
