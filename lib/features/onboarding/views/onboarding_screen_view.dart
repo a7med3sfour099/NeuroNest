3f387d76b9c591b0b62d3343c5a5fc774d791e92
a7med3sfour099 <a7med3sfour099@gmail.com>
2026-06-27 05:04:34 +0300

    Change onboarding design

--- lib/features/onboarding/views/onboarding_screen_view.dart
+++ lib/features/onboarding/views/onboarding_screen_view.dart
@@ -1,3 +1,4 @@
+import 'package:neuronest/core/constants/app_colors.dart';
 import 'package:neuronest/shared/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -15,19 +16,19 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   final List<Map<String, String>> _pages = [
     {
-      'image': 'assets/onboard/onboard_screen.jpg',
-      'title': 'Welcome to SpectrumSense',
+      'image': 'assets/onboard/first_onboarding_image.png',
+      'title': 'Welcome to NeuroNest',
       'subtitle':
           'Helping parents recognize early behavioral signals with care and intelligence.',
     },
     {
-      'image': 'assets/onboard/onboard_screen.jpg',
+      'image': 'assets/onboard/second_onboarding_image.png',
       'title': 'Early Awareness Starts Here',
       'subtitle':
           'Upload a short video and let SpectrumSense provide early insights into your child\'s behavior.',
     },
     {
-      'image': 'assets/onboard/onboard_screen.jpg',
+      'image': 'assets/onboard/third_onboarding_image.jpg',
       'title': 'Smart, Supportive Results',
       'subtitle':
           'Upload a short video and let SpectrumSense provide early insights into your child\'s behavior.',
@@ -42,55 +43,50 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: PageView.builder(
-              controller: _pageController,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: _pages.length,
-              onPageChanged: (index) {
-                setState(() => _currentPage = index);
-              },
-              itemBuilder: (context, index) {
-                return _buildPage(_pages[index]);
-              },
+    return SafeArea(
+      top: false,
+      bottom: false,
+      left: false,
+      right: false,
+      child: Scaffold(
+        backgroundColor: AppColors.primary,
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: _pages.length,
+                onPageChanged: (index) {
+                  setState(() => _currentPage = index);
+                },
+                itemBuilder: (context, index) {
+                  return _buildPage(_pages[index]);
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildPage(Map<String, String> page) {
-    return Column(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(2),
-          child: Image.asset(
-            page['image']!,
-            width: double.infinity,
-            height: 462,
-            fit: BoxFit.cover,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(2),
+            child: Image.asset(
+              page['image']!,
+              width: double.infinity,
+              height: 450,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        Gap(30),
-        Container(
-          width: 370,
-          height: 262,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Color(0xffD9D9D9), width: 1),
-            boxShadow: [
-              BoxShadow(
-                color: Color(0xffD9D9D9),
-                blurRadius: 4,
-                blurStyle: BlurStyle.outer,
-              ),
-            ],
-          ),
-          child: Padding(
+          Gap(25),
+          Padding(
             padding: const EdgeInsets.all(25.0),
             child: Column(
               children: [
@@ -110,7 +106,6 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   textAlign: TextAlign.center,
                   height: 1.7,
                 ),
-                const Spacer(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: List.generate(_pages.length, (index) {
@@ -131,63 +126,62 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               ],
             ),
           ),
-        ),
-        Gap(25),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                backgroundColor: Color(0xff5DB7DE),
-                minimumSize: Size(207, 50),
-              ),
-              onPressed: () {
-                if (_currentPage < _pages.length - 1) {
-                  _pageController.nextPage(
-                    duration: const Duration(milliseconds: 400),
-                    curve: Curves.easeInOut,
-                  );
-                } else {
-                  Navigator.pushReplacementNamed(context, '/login');
-                  // print('Get Started pressed');
-                }
-              },
-              child: CustomText(
-                text: _currentPage < _pages.length - 1 ? 'Next' : 'Get started',
-                size: 23,
-                color: Colors.white,
-                weight: FontWeight.w500,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Gap(31),
 
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                side: BorderSide(color: Color(0xffD9D9D9)),
-                minimumSize: Size(115, 50),
+          Gap(25),
+
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
               ),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/login');
-                // print('Skip pressed');
-              },
-              child: CustomText(
-                text: 'Skip',
-                size: 23,
-                color: Colors.black,
-                weight: FontWeight.w500,
-                textAlign: TextAlign.center,
-              ),
+              backgroundColor: Color(0xff4a7dcd),
+              minimumSize: Size(351, 56),
+              elevation: 8,
+              shadowColor: Colors.blueAccent.withOpacity(0.4),
             ),
-          ],
-        ),
-      ],
+            onPressed: () {
+              if (_currentPage < _pages.length - 1) {
+                _pageController.nextPage(
+                  duration: const Duration(milliseconds: 400),
+                  curve: Curves.easeInOut,
+                );
+              } else {
+                Navigator.pushReplacementNamed(context, '/login');
+              }
+            },
+            child: CustomText(
+              text: _currentPage < _pages.length - 1 ? 'Next' : 'Get started',
+              size: 23,
+              color: Colors.white,
+              weight: FontWeight.w500,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Gap(25),
+
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              side: BorderSide(color: Colors.transparent),
+              minimumSize: Size(351, 56),
+              elevation: 0,
+            ),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/login');
+              // print('Skip pressed');
+            },
+            child: CustomText(
+              text: 'Skip',
+              size: 23,
+              color: Color(0xffa0aec0),
+              weight: FontWeight.w500,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
