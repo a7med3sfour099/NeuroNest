ed76c75e85b26d3fd24d75c2a930d7e24bdfffbc
a7med3sfour099 <a7med3sfour099@gmail.com>
2026-06-27 05:05:00 +0300

    some changes to handle some errors

--- lib/shared/custom_elevatedbutton.dart
+++ lib/shared/custom_elevatedbutton.dart
@@ -2,28 +2,36 @@ import 'package:neuronest/shared/custom_text.dart';
 import 'package:flutter/material.dart';
 
 class CustomElevatedbutton extends StatelessWidget {
-  const CustomElevatedbutton({super.key, required this.text, this.onPressed});
+  const CustomElevatedbutton({
+    super.key,
+    required this.text,
+    this.onPressed,
+    this.child,
+    this.backgroundColor,
+  });
 
   final String text;
   final VoidCallback? onPressed;
+  final Widget? child;
+  final Color? backgroundColor;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        backgroundColor: Color(0xff5DB7DE),
-        minimumSize: Size(double.infinity, 50),
-        // padding: EdgeInsets.symmetric(horizontal: 100, vertical: 6),
+        backgroundColor: backgroundColor ?? const Color(0xff5DB7DE),
+        minimumSize: const Size(double.infinity, 50),
       ),
       onPressed: onPressed,
-      child: CustomText(
-        text: text,
-        size: 23,
-        color: Colors.white,
-        weight: FontWeight.w500,
-        textAlign: TextAlign.center,
-      ),
+      child: child ??
+          CustomText(
+            text: text,
+            size: 23,
+            color: Colors.white,
+            weight: FontWeight.w500,
+            textAlign: TextAlign.center,
+          ),
     );
   }
 }
