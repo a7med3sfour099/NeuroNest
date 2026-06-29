8a24de83cf1ab1673180df758634656e7fa02e1c
a7med3sfour099 <a7med3sfour099@gmail.com>
2026-06-29 14:22:36 +0300

    edit color to app

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,5 +1,31 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class AppColors {
-  static Color primary = Color(0xffFFFFFF);
+  // 1️⃣ Primary Colors
+  static const Color primary = Color(0xff4a7dcd); 
+  
+  // 2️⃣ Secondary Colors
+  static const Color secondary = Color(0xff5DB7DE); 
+  
+  // 3️⃣ Tertiary Colors
+  static const Color background = Color(0xffFFFFFF);
+  static const Color textPrimary = Color(0xff000000);
+  
+  // 4️⃣ Neutral Colors
+  static const Color border = Color(0xffD9D9D9); 
+  static const Color textSecondary = Color(0xff757575);
+  static const Color textTertiary = Color(0xff6c6969);
+  static const Color textQuaternary = Color(0xff9E9E9E);
+
+  // 5️⃣ Status Colors
+  static const Color error = Color(0xffFF0000);
+  static const Color success = Color(0xff00FF00);
+  static const Color warning = Color(0xffFFFF00);
+  static const Color info = Color(0xff0000FF);
+  
+  // 6️⃣ Grayslake Colors
+  static const Color transparent = Colors.transparent;
+  
 }
